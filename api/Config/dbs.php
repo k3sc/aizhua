c592b3ba1341473aadd6d334adf3dba27a4fec4a
wuyujie <coder5@qq.com>
2018-05-27 08:03:20 +0800

    恢复mysql

--- api/Config/dbs.php
+++ api/Config/dbs.php
@@ -9,10 +9,10 @@ return array(
      */
     'servers' => array(
         'db_appapi' => array(                         //服务器标记
-            'host'      => 'rm-wz99ouwa974707w85.mysql.rds.aliyuncs.com',             //数据库域名
-            'name'      => 'aizhuawawajidb',               //数据库名字
-            'user'      => 'aizhuawawajicn',                  //数据库用户名
-            'password'  => 'wawaji123!',	                    //数据库密码
+            'DB_HOST' => 'rm-wz99ouwa974707w85.mysql.rds.aliyuncs.com',
+            'DB_NAME' => 'aizhuawawajidb',
+            'DB_USER' => 'aizhuawawajicn',
+            'DB_PWD' => 'wawaji123!',
             'port'      => '3306',                  //数据库端口
             'charset'   => 'UTF8',                  //数据库字符集
         ),
