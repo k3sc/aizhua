52371daed006f96a63aea4cac6efe618cc175b25
wuyujie <coder5@qq.com>
2018-06-29 23:10:08 +0800

    恢复数据库配置

--- api/Config/dbs.php
+++ api/Config/dbs.php
@@ -10,10 +10,10 @@ return array(
     'servers' => array(
         'db_appapi' => array(                         //服务器标记
 
-            'host'      => '127.0.0.1',             //数据库域名
+            'host'      => 'rm-wz99ouwa974707w85.mysql.rds.aliyuncs.com',             //数据库域名
             'name'      => 'aizhuawawajidb',               //数据库名字
-            'user'      => 'root',                  //数据库用户名
-            'password'  => 'aizhua123!@#Q',                         //数据库密码
+            'user'      => 'aizhuawawajicn',                  //数据库用户名
+            'password'  => 'wawaji123!',                         //数据库密码
             'port'      => '3306',                  //数据库端口
             'charset'   => 'UTF8',                  //数据库字符集
         ),
--- data/conf/db.php
+++ data/conf/db.php
@@ -4,17 +4,17 @@
  */
 return array(
     'DB_TYPE' => 'mysqli',
-    'DB_HOST'      => '127.0.0.1',             //数据库域名
+    'DB_HOST'      => 'rm-wz99ouwa974707w85.mysql.rds.aliyuncs.com',             //数据库域名
     'DB_NAME'      => 'aizhuawawajidb',               //数据库名字
-    'DB_USER'      => 'root',                  //数据库用户名
-    'DB_PWD'  => 'aizhua123!@#Q',                         //数据库密码
+    'DB_USER'      => 'aizhuawawajicn',                  //数据库用户名
+    'DB_PWD'  => 'wawaji123!',                         //数据库密码
 
     'DB_PORT' => '3306',
     'DB_PREFIX' => 'cmf_',
 						
 	/* redis */
 	'REDIS_HOST' => "localhost",
-	'REDIS_AUTH' => "",
+	'REDIS_AUTH' => "rediswawaji123",
 	'REDIS_PREFIX' => "zzwawaji_",
     //密钥
     "AUTHCODE" => 'rCt52pF2cnnKNB3Hkp',
