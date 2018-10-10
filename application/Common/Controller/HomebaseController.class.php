e56005a34958fa86cb5f24711a03109ef9a52277
lenyue <493708759@qq.com>
2018-10-10 14:49:32 +0800

    ~test

--- application/Common/Controller/HomebaseController.class.php
+++ application/Common/Controller/HomebaseController.class.php
@@ -240,7 +240,7 @@ class HomebaseController extends AppframeController {
 
 		$file= str_replace("//",'/',$file);
         echo "<pre>";
-        print_r($file);
+        print_r(L('_TEMPLATE_NOT_EXIST_'));
         exit;
 		if(!file_exists_case($file)) E(L('_TEMPLATE_NOT_EXIST_').':'.$file);
 
