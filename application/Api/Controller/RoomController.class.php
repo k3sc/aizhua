fb543bab9e53c2eb99b9e3e3e484574c451ab299
lenyue <493708759@qq.com>
2018-10-12 15:02:29 +0800

    更正

--- application/Api/Controller/RoomController.class.php
+++ application/Api/Controller/RoomController.class.php
@@ -258,7 +258,7 @@ class RoomController extends BaseController
         M()->query($sql);*/
         //error_log($sql.'\r\n', 3, './data/errors.log');
 
-        M('game_history')->where("id={$id}")->field('continuity')->save($continuity);
+        M('game_history')->where("id={$id}")->save(array('continuity'=>$continuity));
 
         M('game_room')->where('id=' . $room_id)->save(array('status' => 3));
 		$this->notice_gameover('0', '{"type":15,"room_ids":"'.$room_id.'","status":3}');
