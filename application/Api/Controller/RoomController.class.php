b40cc4f703a6a590246bda339180fe25981d7ef2
lenyue <493708759@qq.com>
2018-10-12 15:10:37 +0800

    ~

--- application/Api/Controller/RoomController.class.php
+++ application/Api/Controller/RoomController.class.php
@@ -254,12 +254,12 @@ class RoomController extends BaseController
         $continuity = $this->get_front_with($id);
         //$data['continuity'] = $continuity;
 
-        /*$sql = "update cmf_game_history set continuity={$continuity} where id={$id}";
-        M()->query($sql);*/
+        $sql = "update cmf_game_history set continuity={$continuity} where id={$id}";
+        /*M()->query($sql);*/
         //error_log($sql.'\r\n', 3, './data/errors.log');
-
-//        M('game_history')->where("id={$id}")->save(array('continuity'=>$continuity));
-        M('game_history')->where("id={$id}")->setField('continuity',$continuity);
+        error_log($sql, 3, '/home/wwwroot/default/data/runtime/errors.log');
+        M('game_history')->where("id={$id}")->save(array('continuity'=>$continuity));
+        //M('game_history')->where("id={$id}")->setField('continuity',$continuity);
 
         M('game_room')->where('id=' . $room_id)->save(array('status' => 3));
 		$this->notice_gameover('0', '{"type":15,"room_ids":"'.$room_id.'","status":3}');
